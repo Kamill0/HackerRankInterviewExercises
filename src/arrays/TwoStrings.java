01ac71b6364242fd3d87bd0e2559bfec240945a2
Kamill0 <kamillo1493@gmail.com>
2020-07-17 13:49:09 +0200

    TwoStrings solution.

---
+++ src/arrays/TwoStrings.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoStrings {
+
+    static String twoStrings(String s1, String s2) {
+        Set<String> s1Set = new HashSet<>(Arrays.asList(s1.split("")));
+        for(String s2Letter: s2.split("")) {
+            if(s1Set.contains(s2Letter)) {
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(twoStrings("hello", "world"));
+        System.out.println(twoStrings("hi", "world"));
+    }
+}
