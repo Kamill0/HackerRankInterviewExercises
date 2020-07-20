03e89c3f5c0d0c6a69c7ca1e2769fc341e615351
Kamill0 <kamillo1493@gmail.com>
2020-07-20 13:14:26 +0200

    Removed unused code

--- src/dictionariesandhashmaps/SherlockAndAnagrams.java
+++ src/dictionariesandhashmaps/SherlockAndAnagrams.java
@@ -5,32 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SherlockAndAnagrams {
-
-    private static boolean checkIfAnagram(String a, String b) {
-        if (a.length() != b.length())
-            return false;
-
-        int[] a_freq = getFreqArray(a);
-        int[] b_freq = getFreqArray(b);
-
-        for (int i=0; i<26;++i) {
-            if (a_freq[i] != b_freq[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static int[] getFreqArray(String a) {
-        int [] freq = new int[26];
-        for (int i=0; i<a.length(); ++i) {
-            char c = a.charAt(i);
-            int value = c - 'a';
-            freq[value]++;
-        }
-        return freq;
-    }
-
     static int sherlockAndAnagrams(String s) {
         int anagrams = 0;
         Map<String,Integer> map = new HashMap<>();
@@ -64,7 +38,5 @@ public class SherlockAndAnagrams {
         System.out.println(sherlockAndAnagrams("abba"));
         System.out.println(sherlockAndAnagrams("abcd"));
         System.out.println(sherlockAndAnagrams("cdcd"));
-
-        //System.out.println(checkIfAnagram("abba", "bbba"));
     }
 }
