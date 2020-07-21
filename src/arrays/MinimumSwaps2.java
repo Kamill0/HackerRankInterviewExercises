21eb1635626baeb89e42e57afa475e7cf19d2c0d
Kamill0 <kamillo1493@gmail.com>
2020-07-21 13:59:12 +0200

    MinimumSwaps2 solution

--- src/arrays/MinimumSwaps2.java
+++ src/arrays/MinimumSwaps2.java
@@ -1,4 +1,26 @@
 package arrays;
 
 public class MinimumSwaps2 {
+
+
+    static int minimumSwaps(int[] arr) {
+        int count = 0;
+
+        for(int i=0; i<arr.length; ++i) {
+            while (arr[i] != (i + 1)) {
+                int tmp = arr[i];
+                arr[i] = arr[tmp - 1];
+                arr[tmp - 1] = tmp;
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(minimumSwaps(new int[]{4, 3, 1, 2})); //3
+        System.out.println(minimumSwaps(new int[]{2, 3, 4, 1, 5})); //3
+        System.out.println(minimumSwaps(new int[]{1, 3, 5, 2, 4, 6, 7})); //3
+    }
 }
